1fe44d79b0eaaf0221bd457fbebbdd62a127c21c
rodrigoTerenci <rodrigoterenci@gmail.com>
2023-09-07 18:52:13 +0000

    ee

--- digao-blog/gatsby-config.js
+++ digao-blog/gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Blog do Digão`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Rodrigo Carvalho Terenci`,
+      summary: `who lives and works in São Paulo, the best city in the world, building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
@@ -118,7 +118,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/euFoto.png`, // This path is relative to the root of the site.
       },
     },
   ],
